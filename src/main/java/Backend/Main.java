ab4f3ed52579fe0f87d090ce06a6cb8e10b54bc4
pedro-ueda <pedro.ueda@sptech.school>
2024-10-07 20:34:34 -0300

    introdução da conexão com S3, porém falta acesso

--- src/main/java/Backend/Main.java
+++ src/main/java/Backend/Main.java
@@ -1,8 +1,15 @@
 package Backend;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import S3.S3Provider;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,9 +19,16 @@ public class Main {
         // E o parametro "pathXls" indica a pasta e o nome do arquivo que ele vai criar
         //arquivos.converterCsvToXls("./src/main/java/book.csv","./src/main/java/book.xls");
 
-        arquivos.converterCsvToXls("C:\\SPTech\\Vortexx\\back-end-vortexx\\src\\main\\java\\20240916113529.csv", "C:\\SPTech\\Vortexx\\back-end-vortexx\\src\\main\\java");
+        arquivos.converterCsvToXls("src/main/java/20240916113529.csv", "src/main/java");
 
-        arquivos.lerXls("./src/main/java/s7t5102.xls");
+        arquivos.lerXls("src/main/java/s7t5102.xls");
+
+//        S3Client s3Client = new S3Provider().getS3Client();
+//
+//        List<Bucket> buckets = s3Client.listBuckets().buckets();
+//        for (Bucket bucket : buckets) {
+//            System.out.println("Bucket: " + bucket.name());
+//        }
     }
 }
 
