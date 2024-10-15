65b6e47516ca7be1b9da3810a0d2f832f09f77c3
matheuscastro <matheus.csilva@sptech.school>
2024-10-15 11:50:11 -0300

    Adicionando listagem de todos os arquivos do bucket

--- src/main/java/Backend/Main.java
+++ src/main/java/Backend/Main.java
@@ -2,10 +2,8 @@ package Backend;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.Bucket;
-import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.*;
 import S3.S3Provider;
-import software.amazon.awssdk.services.s3.model.S3Exception;
 
 
 import java.io.IOException;
@@ -21,13 +19,23 @@ public class Main {
 
 //        arquivos.converterCsvToXls("src/main/java/20240916113529.csv", "src/main/java");
 
-        arquivos.lerXls("/home/ubuntu/JAR/s7t5102.xls");
+        //arquivos.lerXls("/home/ubuntu/JAR/s7t5102.xls");
 
         S3Client s3Client = new S3Provider().getS3Client();
 
+        // aqui lista todos os buckets
         List<Bucket> buckets = s3Client.listBuckets().buckets();
         for (Bucket bucket : buckets) {
             System.out.println("Bucket: " + bucket.name());
+        }
+
+        // aqui lista todos os arquivos de um bucket
+        ListObjectsRequest listObjects = ListObjectsRequest.builder()
+                .bucket("") // aqui vai o nome do bucket
+                .build();
+        List<S3Object> objects = s3Client.listObjects(listObjects).contents();
+        for (S3Object object : objects) {
+            System.out.println("Objeto: " + object.key());
         }
     }
 }
--- src/main/java/S3/S3Provider.java
+++ src/main/java/S3/S3Provider.java
@@ -12,12 +12,12 @@ public class S3Provider {
                 System.getenv("AWS_ACCESS_KEY_ID"),
                 System.getenv("AWS_SECRET_ACCESS_KEY"),
                 System.getenv("AWS_SESSION_TOKEN")
-        );
+                );
     }
 
     public S3Client getS3Client() {
         return S3Client.builder()
-                .region(Region.SA_EAST_1)
+                .region(Region.US_EAST_1)
                 .credentialsProvider(() -> credentials)
                 .build();
     }
