c2bd70c00437ff1ea828eeb71d1ed278bfc89a6e
pedro-ueda <pedro.ueda@sptech.school>
2024-11-11 19:55:10 -0300

    adicionando a classe Empresa

---
+++ src/main/java/Backend/Objetos/Empresa.java
@@ -0,0 +1,51 @@
+package Backend.Objetos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Empresa{
+    private String cpnj;
+    private Gestor gestor;
+    private List<Funcionario> funcionarios = new ArrayList();
+    private List<Gestor> gestores = new ArrayList();
+
+    public Empresa(String cpnj, Gestor gestor) {
+        this.cpnj = cpnj;
+        this.gestor = gestor;
+    }
+
+    public Empresa() {
+    }
+
+    public void adicionarGestor(Gestor gestor) {
+        gestores.add(gestor);
+    }
+
+    public void adicionarFuncionario(Funcionario funcionario) {
+        funcionarios.add(funcionario);
+    }
+
+    public String getCpnj() {
+        return cpnj;
+    }
+
+    public void setCpnj(String cpnj) {
+        this.cpnj = cpnj;
+    }
+
+    public Gestor getGestor() {
+        return gestor;
+    }
+
+    public void setGestor(Gestor gestor) {
+        this.gestor = gestor;
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+}
